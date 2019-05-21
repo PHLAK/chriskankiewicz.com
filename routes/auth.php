f303adf4b23746e88ef9e8c7d0afbe573c36b40e
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-05-20 21:22:21 -0700

    More auth route fixes

--- routes/auth.php
+++ routes/auth.php
@@ -13,13 +13,12 @@ use App\Http\Controllers\Auth;
 
 Route::get('login', [Auth\LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [Auth\LoginController::class, 'login']);
-
 Route::post('logout', [Auth\LoginController::class, 'logout'])->name('logout');
 
-Route::post('password/email', [Auth\ForgotPassowordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset', [Auth\ForgotPassowordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset', [Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/reset', [Auth\ResetPasswordController::class, 'reset'])->name('password.update');
-Route::get('password/reset/{token}', [Auth\ResetPasswordController::class, 'shoResetForm'])->name('password.reset');
+Route::get('password/reset/{token}', [Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 
 Route::get('register', [Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [Auth\RegisterController::class, 'register']);
