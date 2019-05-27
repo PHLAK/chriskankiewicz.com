707ecada1f113719e4ca9259f28901dff16227f3
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-05-27 00:44:12 -0700

    Initial Vue.js component work

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -17,10 +16,10 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
- // const files = require.context('./', true, /\.vue$/i);
- // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('education-component', require('./components/EducationComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/HttpRequestComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="rounded-lg border-2 border-gray-800 shadow-solid-gray-800 overflow-hidden w-full my-8">
+        <div class="flex justify-between items-center bg-gray-800 border-b-2 border-gray-800 p-4">
+            <span class="font-mono text-white text-xl">
+                GET {{ requestUri }}
+            </span>
+
+            <button v-on:click="makeRequest" class="bg-teal-600 px-4 py-2 rounded text-white hover:bg-teal-700">
+                Fetch {{ title }}
+            </button>
+        </div>
+
+        <textarea v-model="responseBody" class="font-mono p-4 w-full h-64" readonly></textarea>
+
+        <div class="bg-gray-200 border-t-2 border-gray-800 p-4 text-center">
+            <a :href="docsHref" class="text-sm underline">
+                View {{ title }} Documentation
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                responseBody: this.$slots.default[0].text.trim()
+            }
+        },
+        computed: {
+            docsHref: function () {
+                return '/docs#tag/' + this.title;
+            }
+        },
+        props: ['request-uri', 'title'],
+        methods: {
+            makeRequest: function () {
+                axios.get(this.requestUri).then(
+                    response => this.responseBody = JSON.stringify(response.data, null, 2)
+                ).catch(
+                    response => console.log(response)
+                );
+            }
+        }
+    }
+</script>
