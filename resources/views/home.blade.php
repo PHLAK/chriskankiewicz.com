f1eece73dfc2f80cc2a70df53df76a8bcd858981
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-06-20 22:14:11 -0700

    Updated logged in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
-                    </div>
-                </div>
+    <div class="flex content-center justify-center items-center h-screen bg-gray-200">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="text-center">
+            {{ csrf_field() }}
+
+            <h1 class="text-2xl pb-4">You are logged in!</h1>
+
+            <div class="pb-4">
+                <a href="{{ route('index') }}">Home</a>
+                &bull; <a href="{{ route('wink.pages.index') }}">Blog</a>
+                &bull; <a href="{{ route('telescope') }}">Telescope</a>
+                &bull; <a href="{{ route('nova.login') }}">Nova</a>
+            </div>
+
+            <button type="submit" class="rounded bg-teal-600 text-white px-4 py-2">Logout</button>
+        </form>
     </div>
 @endsection
