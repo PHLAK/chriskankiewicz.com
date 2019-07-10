d9209e2d19afa12a2fc945b1ba71610be5e2df3f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-07-09 21:42:43 -0700

    Redesigned headings

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -1,5 +1,5 @@
 <{{ $tag ?? 'h1' }} class="mb-6">
-    <span class="inline-block bg-gray-800 font-mono text-white text-2xl px-4 py-2">
+    <span class="inline-block border-2 border-gray-800 font-mono text-gray-800 text-2xl shadow-solid-gray-800 px-4 py-2">
         {{ $slot }}
     </span>
 </{{ $tag ?? 'h1' }}>
