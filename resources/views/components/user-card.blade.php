53400f6a430bd3241b5856f6b239535d8db5b51e
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-07-10 21:33:45 -0700

    User card updates

--- resources/views/components/user-card.blade.php
+++ resources/views/components/user-card.blade.php
@@ -3,18 +3,21 @@
 
     <img src="{{ gravatar('Chris@ChrisKankiewicz.com', 300) }}" alt="Profile Photo" class="w-32 rounded-full border-white border-8 mx-4 -mt-16 md:w-24 md:-mt-12 lg:w-32 lg:-mt-16">
 
-    <h1 class="font-semibold text-xl -mt-16 ml-32 px-8 py-2 md:text-lg md:-mt-12 md:ml-24 lg:-mt-16 lg:text-xl lg:ml-32">
-        Chris Kankiewicz
+    <div class="-mt-16 ml-32 px-6 py-2 md:-mt-12 md:ml-24 lg:-mt-16 lg:ml-32">
+        <h1 class="font-semibold text-xl lg:text-2xl">
+            Chris Kankiewicz
+        </h1>
 
         <div class="font-light text-base text-gray-500 md:text-sm lg:text-base">
             <i class="far fa-map-marker-alt fa-fw"></i>
 
             <a href="https://goo.gl/maps/Y5xrX7AQ9RUNpCKo8" class="hover:underline">Mesa, Arizona</a>
         </div>
-    </h1>
+    </div>
 
     <p class="mb-4 px-4 py-2 md:text-sm lg:text-base">
         Passionate PHP developer, Linux junkie, gamer and coffee afficionado.
+        Dedicated husband and father of two.
     </p>
 
     <div class="flex justify-around p-4 border-t-2 border-gray-800">
@@ -22,19 +25,19 @@
             <i class="fab fa-github fa-fw fa-2x"></i>
         </a>
 
-        <a href="#" title="Twitter" class="text-twitter">
+        <a href="https://twitter.com/PHLAK" title="Twitter" class="text-twitter">
             <i class="fab fa-twitter fa-fw fa-2x"></i>
         </a>
 
-        <a href="#" title="LinkedIn" class="text-linkedin">
+        <a href="https://www.linkedin.com/in/chriskankiewicz/" title="LinkedIn" class="text-linkedin">
             <i class="fab fa-linkedin fa-fw fa-2x"></i>
         </a>
 
-        <a href="#" title="Keybase" class="text-keybase">
+        <a href="https://keybase.io/chriskankiewicz" title="Keybase" class="text-keybase">
             <i class="fab fa-keybase fa-fw fa-2x"></i>
         </a>
 
-        <a href="#" title="Steam" class="text-steam">
+        <a href="https://steamcommunity.com/id/phlak" title="Steam" class="text-steam">
             <i class="fab fa-steam fa-fw fa-2x"></i>
         </a>
     </div>
