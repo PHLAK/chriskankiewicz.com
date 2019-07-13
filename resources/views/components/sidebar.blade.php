7e461a0670e6373227d25bc06bdc9bb2294d10bb
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-07-12 23:20:07 -0700

    Updated sidebar footer info

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,17 @@
 <div id="sidebar" class="flex flex-col justify-between p-6 bg-gray-200 h-full md:max-w-md">
     @include('components.user-card')
 
-    <a href="https://github.com/PHLAK/chriskankiewicz.com" class="block bg-gray-800 text-white text-center p-4 mt-6 -mx-6 -mb-6 sticky bottom-0 hover:bg-gray-700">
-        <i class="far fa-code fa-fw"></i> View source on GitHub
-    </a>
+    <div class="block bg-gray-800 text-white text-center p-4 mt-6 -mx-6 -mb-6 sticky bottom-0">
+        <a href="mailto:Chris@ChrisKankiewicz.com" class="hover:underline">
+            Chris@ChrisKankiewicz.com
+        </a>
+
+        <span class="mx-1">
+            &bull;
+        </span>
+
+        <a href="tel:623-252-6855">
+            623-252-6855
+        </a>
+    </div>
 </div>
