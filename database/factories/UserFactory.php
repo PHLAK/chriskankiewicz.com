7c402920258b037223f0409aaef6c3b7405687b6
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-08-03 21:29:39 -0700

    Additional test updates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,11 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'api_token' => Str::random(60)
+        'api_token' => Str::random(60),
+        'is_admin' => false
     ];
+});
+
+$factory->state(User::class, 'is_admin', function (Faker $faker) {
+    return ['is_admin' => true];
 });
--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    public function test_it_can_acces_the_registration_page()
+    use RefreshDatabase;
+
+    public function test_it_can_not_acces_the_registration_page()
     {
         $response = $this->get(route('register'));
 
@@ -18,5 +22,27 @@ class AuthTest extends TestCase
         $response = $this->get(route('login'));
 
         $response->assertOk();
+    }
+
+    public function test_it_can_log_in()
+    {
+        $user = factory(User::class)->states('is_admin')->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect(route('dashboard'));
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_it_redirects_to_the_dashboard_when_already_logged_in()
+    {
+        $user = factory(User::class)->states('is_admin')->make();
+
+        $response = $this->actingAs($user)->get(route('login'));
+
+        $response->assertRedirect(route('dashboard'));
     }
 }
