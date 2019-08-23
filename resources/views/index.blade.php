f821b61fa2386a16122a7358fe01462fa4ad8060
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-08-23 08:07:31 -0700

    Added horizontal rules between sections

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
     @include('components.skills')
+    <hr class="border-t-4 border-teal-600 w-40 my-16">
     @include('components.experience')
+    <hr class="border-t-4 border-teal-600 w-40 my-16">
     @include('components.education')
+    <hr class="border-t-4 border-teal-600 w-40 my-16">
     @include('components.projects')
+    <hr class="border-t-4 border-teal-600 w-40 my-16">
     @include('components.accomplishments')
 
-    <footer class="border-t border-gray-500 text-center text-gray-500 text-sm pt-6">
+    <footer class="border-t border-gray-500 text-center text-gray-500 text-sm mt-16 pt-6">
         &copy; {{ date('Y') }} Chris Kankiewicz
 
         <span class="mx-1">
