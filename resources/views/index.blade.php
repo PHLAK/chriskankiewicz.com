23de531b529d5fbc50b050766668a767becbf3be
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-09-02 00:54:43 -0700

    Footer style improvements

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,14 +7,14 @@
     @include('components.projects')
     @include('components.accomplishments')
 
-    <footer class="border-t border-gray-500 text-center text-gray-500 text-sm mt-16 pt-6">
+    <footer class="bg-gray-200 text-center text-gray-600 p-4 -mx-6 -mb-6">
         &copy; {{ date('Y') }} Chris Kankiewicz
 
         <span class="mx-1">
             &bull;
         </span>
 
-        <a href="https://github.com/PHLAK/chriskankiewicz.com" class="hover:underline">
+        <a href="https://github.com/PHLAK/chriskankiewicz.com" class="underline">
             View source on GitHub
         </a>
     </footer>
