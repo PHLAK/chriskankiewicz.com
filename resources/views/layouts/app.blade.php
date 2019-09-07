68d2f664e8c8b95d4075b9b5acdc0f4563f8414c
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-09-06 20:40:46 -0700

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <script src="{{ mix('js/app.js') }}" defer></script>
-<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+<link rel="icon" href="{{ asset('favicon.png') }}">
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
 
 <title>Chris Kankiewicz</title>
 
