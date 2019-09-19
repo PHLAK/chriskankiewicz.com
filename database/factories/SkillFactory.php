baf6447195daa21149248500b0bfeab87d9a1e67
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-09-18 19:42:23 -0700

    Generate unique names in SkillFactory

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Skill::class, function (Faker $faker) {
     return [
-        'name' => $faker->word(),
+        'name' => $faker->unique()->word(),
         'emphasis' => $faker->numberBetween(-1, 1)
     ];
 });
