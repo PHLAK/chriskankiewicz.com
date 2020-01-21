1caf766df93457c035dfc3d9a36796214eccee03
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-01-20 19:38:13 -0700

    Import Route facade

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
