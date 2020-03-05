3df6675195a08782dd7e611b650fadafbe35aed6
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-03-04 22:05:28 -0700

    Removed Canvas related files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers;
-use Canvas\Http\Middleware\Session;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,15 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Controllers\IndexController::class)->name('index');
-
-Route::prefix('blog')->group(function () {
-    Route::get('/', [Controllers\BlogController::class, 'getPosts'])->name('blog.index');
-    Route::get('{slug}', [
-        Controllers\BlogController::class, 'findPostBySlug'
-    ])->middleware(Session::class)->name('blog.post');
-    Route::get('tag/{slug}', [Controllers\BlogController::class, 'getPostsByTag'])->name('blog.tag');
-    Route::get('topic/{slug}', [Controllers\BlogController::class, 'getPostsByTopic'])->name('blog.topic');
-});
 
 Route::get('/dashboard', [Controllers\DashboardController::class, 'index'])->name('dashboard');
 
