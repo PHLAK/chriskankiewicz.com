f73ec1441da40678678ec608862d6a5340454a4e
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-11-17 21:12:01 -0700

    Use App::isLocal() for environment check

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -42,9 +42,9 @@ class CreateUsersTable extends Migration
             'email' => 'Chris@ChrisKankiewicz.com',
             'email_verified_at' => Carbon::now(),
             'password' => Hash::make(
-                config('app.env') == 'local' ? 'secret' : Str::random(32)
+                App::isLocal() ? 'secret' : Str::random(32)
             ),
-            'api_token' => config('app.env') == 'local' ? 'TEST_TOKEN_PLEASE_IGNORE' : Str::random(60),
+            'api_token' => App::isLocal() ? 'TEST_TOKEN_PLEASE_IGNORE' : Str::random(60),
         ]);
 
         $user->is_admin = true;
