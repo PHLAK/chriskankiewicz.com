522effa2eb09d251960e21b3aaaf853788a118c0
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-11-21 23:07:32 -0700

    Made footer sticky

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,18 +3,18 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<script src="{{ mix('js/app.js') }}" defer></script>
+
 <link rel="icon" href="{{ asset('favicon.png') }}">
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 <title>{{ isset($title) ? sprintf('%s • ', $title) : null }}Chris Kankiewicz</title>
 
-<div id="app" class="font-sans antialiased">
+<div id="app" class="flex flex-col min-h-screen font-sans antialiased">
     <x-navigation />
 
     @yield('before-content')
 
-    <div class="px-4 py-8 sm:px-6">
+    <div class="flex-grow px-4 py-8 sm:px-6">
         <div id="content" class="container mx-auto lg:max-w-3xl">
             @yield('content')
         </div>
@@ -22,3 +22,5 @@
 
     <x-footer />
 </div>
+
+<script src="{{ mix('js/app.js') }}" defer></script>
