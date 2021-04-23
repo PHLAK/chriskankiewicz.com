b6a5333e5023c319ce6108f196dcd731e8f2e732
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-04-22 21:28:44 -0700

    Removed unused .eslintrc.json file and updated eslint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     'env': {
         'amd': true,
         'browser': true,
-        'es2021': true
+        'es2021': true,
+        'node': true
     },
     'extends': [
         'eslint:recommended',
