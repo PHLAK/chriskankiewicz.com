d9fa4ab40b2780a94c451326c774256cba19e9dd
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-09-27 10:22:15 -0700

    Updated to Laravel 11

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,23 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /** @extends Factory<\App\User> */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string,mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
             'api_token' => Str::random(60),
             'is_admin' => false,
