32e2c061a318f286ef2d5f4bfeedc6e68ab45aab
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2024-11-27 09:39:04 -0700

    Added umami event tracking to footer links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,35 +1,35 @@
 <footer>
     <div class="bg-gray-100 px-4 py-8 sm:px-6">
         <div class="container flex justify-center space-x-4 sm:space-x-6 mx-auto">
-            <a href="https://github.com/PHLAK" title="GitHub" class="text-github transition-all hover:scale-125">
+            <a href="https://github.com/PHLAK" title="GitHub" class="text-github transition-all hover:scale-125" data-umami-event="github-link">
                 <i class="fab fa-github fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://bsky.app/profile/phlak.dev" title="Bluesky" class="text-bluesky transition-all hover:scale-125">
+            <a href="https://bsky.app/profile/phlak.dev" title="Bluesky" class="text-bluesky transition-all hover:scale-125" data-umami-event="bluesky-link">
                 <i class="fab fa-bluesky fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://mastodon.social/@PHLAK" title="Mastodon" class="text-mastodon transition-all hover:scale-125">
+            <a href="https://mastodon.social/@PHLAK" title="Mastodon" class="text-mastodon transition-all hover:scale-125" data-umami-event="mastodon-link">
                 <i class="fab fa-mastodon fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://www.reddit.com/user/PHLAK" title="Reddit" class="text-reddit transition-all hover:scale-125">
+            <a href="https://www.reddit.com/user/PHLAK" title="Reddit" class="text-reddit transition-all hover:scale-125" data-umami-event="reddit-link">
                 <i class="fab fa-reddit fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://twitter.com/PHLAK" title="Twitter" class="text-twitter transition-all hover:scale-125">
+            <a href="https://twitter.com/PHLAK" title="Twitter" class="text-twitter transition-all hover:scale-125" data-umami-event="twitter-link">
                 <i class="fab fa-twitter fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/chriskankiewicz/" title="LinkedIn" class="text-linkedin transition-all hover:scale-125">
+            <a href="https://www.linkedin.com/in/chriskankiewicz/" title="LinkedIn" class="text-linkedin transition-all hover:scale-125" data-umami-event="linkedin-link">
                 <i class="fab fa-linkedin fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://keybase.io/chriskankiewicz" title="Keybase" class="text-keybase transition-all hover:scale-125">
+            <a href="https://keybase.io/chriskankiewicz" title="Keybase" class="text-keybase transition-all hover:scale-125" data-umami-event="keybase-link">
                 <i class="fab fa-keybase fa-fw fa-2x"></i>
             </a>
 
-            <a href="https://steamcommunity.com/id/phlak" title="Steam" class="text-steam transition-all hover:scale-125">
+            <a href="https://steamcommunity.com/id/phlak" title="Steam" class="text-steam transition-all hover:scale-125" data-umami-event="steam-link">
                 <i class="fab fa-steam fa-fw fa-2x"></i>
             </a>
         </div>
@@ -38,13 +38,13 @@
     <div class="bg-gray-800 px-4 py-8 sm:px-6">
         <div class="container text-center text-gray-300 space-y-2 mx-auto">
             <div>
-                <a href="mailto:Chris@ChrisKankiewicz.com" class="inline-block hover:underline">
+                <a href="mailto:Chris@ChrisKankiewicz.com" class="inline-block hover:underline" data-umami-event="email-link">
                     Chris@ChrisKankiewicz.com
                 </a>
 
                 <span class="mx-1">&bull;</span>
 
-                <a href="tel:623-252-6855" class="inline-block">
+                <a href="tel:623-252-6855" class="inline-block" data-umami-event="phone-link">
                     623-252-6855
                 </a>
             </div>
@@ -56,7 +56,7 @@
 
                 <span class="mx-1">&bull;</span>
 
-                <a href="https://github.com/PHLAK/chriskankiewicz.com" class="underline">
+                <a href="https://github.com/PHLAK/chriskankiewicz.com" class="inline-block underline" data-umami-event="view-source-link">
                     View Source
                 </a>
             </div>
