a5662b8754c3602c9d79d6151be18708f568f80e
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-08 22:40:39 -0700

    Added stdClass to serializable_classes

--- config/cache.php
+++ config/cache.php
@@ -17,7 +17,7 @@ return [
 
     'default' => env('CACHE_STORE', 'database'),
 
-    'serializable_classes' => false,
+    'serializable_classes' => [stdClass::class],
 
     /*
     |--------------------------------------------------------------------------
