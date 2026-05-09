bb2cf58c929272eb26d121551a71d3ee22ce4a5a
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-08 21:10:58 -0700

    Updated to Laraval 13

--- config/cache.php
+++ config/cache.php
@@ -17,6 +17,8 @@ return [
 
     'default' => env('CACHE_STORE', 'database'),
 
+    'serializable_classes' => false,
+
     /*
     |--------------------------------------------------------------------------
     | Cache Stores
