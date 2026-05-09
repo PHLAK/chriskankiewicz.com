a31db1219ec0e541cd046074abe427e154787ebc
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-08 23:23:24 -0700

    Minor cleanup to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: {
         host: true,
-        port: 5173
+        port: 5173,
+        allowedHosts: true,
     },
     plugins: [
         laravel({
