0d1ffe4569b03b92dd030bdddb31a568ce4c456a
Chris Kankiewicz <Chris@Kankiewicz.com>
2026-05-09 18:42:38 -0700

    Added hmr.host to Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        allowedHosts: true,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        }
     },
     plugins: [
         laravel({
